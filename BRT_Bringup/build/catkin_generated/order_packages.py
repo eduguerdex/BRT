0d5675ab13cdbd8b90b9d80fdf7e01ec165414fb
eduguerdex <77515405+eduguerdex@users.noreply.github.com>
2021-09-29 18:36:46 -0500

    Add files via upload

---
+++ BRT_Bringup/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/user/lab_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/user/lab_ws/devel;/home/user/catkin_ws/devel;/home/user/simulation_ws/devel;/home/simulations/public_sim_ws/devel;/opt/ros/melodic'.split(';') if '/home/user/lab_ws/devel;/home/user/catkin_ws/devel;/home/user/simulation_ws/devel;/home/simulations/public_sim_ws/devel;/opt/ros/melodic' != '' else []
